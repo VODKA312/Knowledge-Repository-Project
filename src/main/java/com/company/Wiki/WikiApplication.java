83a06dbc51632ab2fe9a440ce7db31e4a4902c53
VODKA312 <15356203871@163.com>
2021-05-20 16:49:43 +0800

    5-20 修改启动类（打印一些信息） 修改启动端口

--- src/main/java/com/company/Wiki/WikiApplication.java
+++ src/main/java/com/company/Wiki/WikiApplication.java
@@ -1,14 +1,28 @@
 package com.company.Wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WikiApplication {
 
+	private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
 	public static void main(String[] args) {
 
-		SpringApplication.run(WikiApplication.class, args);
+		//SpringApplication.run(WikiApplication.class, args);
+		SpringApplication app = new SpringApplication(WikiApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		/**
+		 * 打印显示启动成功，并且显示地址
+		 */
+		LOG.info("启动成功！！！");
+		LOG.info("地址:\thttp://127.0.0.1:{}",env.getProperty("server.port"));
+		/**
+		 * 打印自定义配置信息，欢迎页，测试地址，提示性方案等
+		 */
 	}
 
 }
