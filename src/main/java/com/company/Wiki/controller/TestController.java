7d11fcdc7506848b79c53a2a90ddc0f7d63c7372
VODKA312 <15356203871@163.com>
2021-05-22 16:30:33 +0800

    5-22 【自定义配置项】可以写在properties文件夹下，controller可以读到 【http测试】解决了http测试问题，高版本tomcat不兼容原有的http测试用例 【Controller】可以打印在properties中的值，且读到值的优先级高于默认值

--- src/main/java/com/company/Wiki/controller/TestController.java
+++ src/main/java/com/company/Wiki/controller/TestController.java
@@ -1,11 +1,18 @@
 package com.company.Wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 //用来返回字符串 @RestController
 //用来返回页面 @Controller 前后端分离的项目基本上不用
 @RestController
 public class TestController {
+
+    //在resource下的properties文件下读自定义配置项的值
+    //:value 默认配置项为value
+    //优先级：配置项读到的值 > 默认配置项值
+    @Value("${test.hello:TEST}")
+    private String TestHello;
 
     /**
      * 地址：http://127.0.0.1:8088/hello
@@ -14,7 +21,7 @@ public class TestController {
      */
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World";
+        return "Hello World " + TestHello; //读取值
     }
 
     @PostMapping("/hello/name")
