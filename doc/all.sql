2e3626caac6cddab992d9689d74ae8f32a23b60f
VODKA312 <15356203871@163.com>
2021-06-02 20:48:37 +0800

    6-2【更新日志】新增ebook表，并用代码生成器自动生成持久层代码

--- doc/all.sql
+++ doc/all.sql
@@ -18,4 +18,24 @@ create table `demo` (
                         primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment ='测试';
 
-insert into `demo` (id, name, password) VALUES (1,'yytdev','0312')
+insert into `demo` (id, name, password) VALUES (1,'yytdev','0312');
+/**
+  增加电子书表
+ */
+drop table if exists `ebook`;
+create table `ebook` (
+                        `id` bigint not null comment 'id',
+                        `name` varchar(50) comment '名称',
+                        `category1_id` bigint comment '一级分类',
+                        `category2_id` bigint comment '二级分类',
+                        `description` varchar(200) comment '描述',
+                        `cover` varchar(200) comment '封面',
+                        `doc_count` int comment '文档数',
+                        `view_count` int comment '阅读数',
+                        `vote_count` int comment '点赞数',
+                        primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment ='电子书';
+
+insert into `ebook` (id, name, description) VALUES (1,'操作系统算法测试1','测试文档1');
+insert into `ebook` (id, name, description) VALUES (2,'操作系统算法测试2','测试文档2');
+insert into `ebook` (id, name, description) VALUES (3,'操作系统算法测试3','测试文档3');
