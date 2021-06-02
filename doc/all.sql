df8fc784c3aaacfd91993af5c1a150ab2b2169ad
VODKA312 <15356203871@163.com>
2021-06-02 20:15:33 +0800

    6-2【更新日志】集成mybatics-generter插件 生成demo表代码

--- doc/all.sql
+++ doc/all.sql
@@ -6,4 +6,16 @@ create table `test` (
     primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment ='测试';
 
-insert into `test` (id, name, password) VALUES (1,'yyt','0312')
+insert into `test` (id, name, password) VALUES (1,'yyt','0312');
+/**
+  增加demo表
+ */
+drop table if exists `demo`;
+create table `demo` (
+                        `id` bigint not null comment 'id',
+                        `name` varchar(50) comment '名称',
+                        `password` varchar(50) comment '密码',
+                        primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment ='测试';
+
+insert into `demo` (id, name, password) VALUES (1,'yytdev','0312')
