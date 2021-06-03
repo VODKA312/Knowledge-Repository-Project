716e2b30b31fc309a4ce1a07fb41085d8a0ff6f3
VODKA312 <15356203871@163.com>
2021-06-03 12:22:12 +0800

    6-3【更新日志】制作CopyUtils类封装BeanUtils 简化开发

--- src/main/java/com/company/Wiki/service/EbookService.java
+++ src/main/java/com/company/Wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.company.Wiki.domain.EbookExample;
 import com.company.Wiki.mapper.EbookMapper;
 import com.company.Wiki.req.EbookReq;
 import com.company.Wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.company.Wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -28,13 +27,22 @@ public class EbookService {
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
         //循环ebooklist里的实体，让他转换为EbookResp
 
+        /**
         List<EbookResp> respList = new ArrayList<>();
         for (Ebook ebook : ebookList) {
+            //原先用BeanUtils的方式进行赋值
             EbookResp ebookResp = new EbookResp();
             //使用Utils工具类拷贝属性
             BeanUtils.copyProperties(ebook,ebookResp);
+            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
             respList.add(ebookResp);
         }
-        return respList;
+        */
+
+        /**
+         * 使用列表复制功能，简化for循环
+         */
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+        return list;
     }
 }
---
+++ src/main/java/com/company/Wiki/util/CopyUtil.java
@@ -0,0 +1,45 @@
+package com.company.Wiki.util;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CopyUtil {
+
+    /**
+     * 单体复制
+     * 与原来的写法不同的是，输入的参数 目标对象->目标类
+     */
+    public static <T> T copy(Object source, Class<T> clazz) {
+        //对源进行判断，如果源为空，则返回空
+        if (source == null) {
+            return null;
+        }
+        T obj = null;
+        try {
+            obj = clazz.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        BeanUtils.copyProperties(source, obj);
+        return obj;
+    }
+
+    /**
+     * 列表复制
+     */
+    public static <T> List<T> copyList(List source, Class<T> clazz) {
+        //1. new一个List
+        List<T> target = new ArrayList<>();
+        if (!CollectionUtils.isEmpty(source)){
+            for (Object c: source) {
+                T obj = copy(c, clazz);
+                target.add(obj);
+            }
+        }
+        return target;
+    }
+}
