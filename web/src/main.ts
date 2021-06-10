189ef3c2cad8c950d338a964b09df87477c0c437
VODKA312 <15356203871@163.com>
2021-06-10 23:00:34 +0800

    6-3【更新日志】【前端】1.修改代码规范格式 2.安装图标库 3.全局引入图标库

--- web/src/main.ts
+++ web/src/main.ts
@@ -5,4 +5,12 @@ import store from './store'
 //引入ant-desgin-vue
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
-createApp(App).use(store).use(router).use(Antd).mount('#app');
+//引入所有图标库
+import * as Icons from '@ant-design/icons-vue';
+const app = createApp(App);
+app.use(store).use(router).use(Antd).mount('#app');
+// 全局使用图标
+const icons: any = Icons;
+for (const i in icons) {
+    app.component(i, icons[i]);
+}
