3bc44c573fcb06ec2aa9d9e97442179737afb59e
VODKA312 <15356203871@163.com>
2021-06-11 15:44:53 +0800

    6-3【更新日志】【后端】1.增加AOP整合拦截器和过滤器 2.增加依赖

--- src/main/java/com/company/Wiki/config/SpringMvcConfig.java
+++ src/main/java/com/company/Wiki/config/SpringMvcConfig.java
@@ -1,10 +1,6 @@
 package com.company.Wiki.config;
-import com.company.Wiki.interceptor.LogInterceptor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
 
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
@@ -13,13 +9,13 @@ public class SpringMvcConfig implements WebMvcConfigurer {
     /**
      * 注入打印日志的拦截器
      */
-    @Resource
-    LogInterceptor logInterceptor;
-
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**")//加入所有请求进行过滤
-                .excludePathPatterns("/login"); //排除/login这个请求，不过滤
-    }
+//    @Resource
+//    LogInterceptor logInterceptor;
+//
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**")//加入所有请求进行过滤
+//                .excludePathPatterns("/login"); //排除/login这个请求，不过滤
+//    }
 }
